084318ade5ed1ccac0ca19ae23e7e29221228aff
ms-cmy <matheus4526souza@gmail.com>
2024-12-07 23:00:53 -0300

    updating

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from main import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_health_response(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.data == b'OK'
+    return 
