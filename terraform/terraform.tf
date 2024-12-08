75ebe529d30ed4aa35f56357069712a81ea573e2
ms-cmy <matheus4526souza@gmail.com>
2024-12-08 15:21:19 -0300

    updating

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -90,3 +90,18 @@ resource "google_cloud_run_v2_service" "default" {
       }
     }
   }
+
+resource "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers"
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_policy" "noauth" {
+  name = google_cloud_run_v2_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+  project = google_cloud_run_v2_service.default.project
+}
