c18f727a5b2d775341c8b35592e51a05c096b073
ms-cmy <matheus4526souza@gmail.com>
2024-12-08 10:52:15 -0300

    updating

--- terraform/artifact_registry/artifact_registry.tf
+++ terraform/artifact_registry/artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "docker_repo" {
   location          = "us-central1" 
-  repository_id     = "mycloudrun-registry-docker" 
+  repository_id     = "mycloudrunregistrydocker" 
   format            = "DOCKER"
 
   cleanup_policies {
