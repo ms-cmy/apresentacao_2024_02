536f3e38afb9f068829a5915cc0aa02681a823c9
ms-cmy <matheus4526souza@gmail.com>
2024-12-08 12:20:09 -0300

    updating

--- terraform/bigquery/bq/bigquery.tf
+++ terraform/bigquery/bq/bigquery.tf
@@ -1,7 +1,3 @@
-resource "google_pubsub_topic" "default" {
-  name = "topic-project-titanic"
-}
-
 resource "google_bigquery_table" "default" {
   dataset_id = "titanic_results"
   table_id   = "titanic"
@@ -25,15 +21,4 @@ resource "google_bigquery_table" "default" {
   }
 ]
 EOF
-}
-
-resource "google_pubsub_subscription" "default" {
-  name  = "publish to bigquery titanic"
-  topic = google_pubsub_topic.default.name
-
-  bigquery_config {
-    table = "${google_bigquery_table.default.dataset_id}.${google_bigquery_table.default.table_id}"
-    # use_topic_schema = true  
-    write_metadata = true    
-  }
 }
