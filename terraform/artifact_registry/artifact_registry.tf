cd16a3a20504724b76f40d5e95354197e470c1ff
ms-cmy <matheus4526souza@gmail.com>
2024-12-08 11:02:05 -0300

    updating

--- terraform/artifact_registry/artifact_registry.tf
+++ terraform/artifact_registry/artifact_registry.tf
@@ -4,7 +4,7 @@ resource "google_artifact_registry_repository" "mycloudrun-repo" {
   format            = "DOCKER"
 
   cleanup_policies {
-    id = "keep only 1 image"
+    id = "keep-only-1-image"
     most_recent_versions {
       keep_count = 1
     }
