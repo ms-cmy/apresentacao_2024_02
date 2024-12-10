b706b133ecbcb69bbdffb5fe3a2e707675a2e338
ms-cmy <matheus4526souza@gmail.com>
2024-12-10 14:48:28 -0300

    updating

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -96,23 +96,7 @@ resource "google_cloud_run_v2_service" "cloud_run" {
   lifecycle {
     ignore_changes = [ template[0].containers[0].image ]
   }
-  }
-
-# data "google_iam_policy" "noauth" {
-#   binding {
-#     role = "roles/run.invoker"
-#     members = [
-#       "allUsers"
-#     ]
-#   }
-# }
-
-# resource "google_cloud_run_v2_service_iam_policy" "cloud_run" {
-#   name = "//run.googleapis.com/projects/${var.project_id}/locations/us-central1/services/${google_cloud_run_v2_service.default.name}"
-#   policy_data = data.google_iam_policy.noauth.policy_data
-#   project = var.project_id
-#   location = "us-central1"
-# }
+}
 
 
 resource "google_cloud_run_service_iam_member" "member" {
